835bb2cf5bd9ad7896be0cdfd7a16e67272af9d5
ADITYA DEY <deyaditya2004@gmail.com>
2023-12-06 02:02:45 +0530

    IT IS DATA TYPES

---
+++ 2. data types/1.js
@@ -0,0 +1,26 @@
+"use strict"; // TREAT ALL JS CODE AS NEWER VERSION
+
+let name = "ADITYA DEY"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+
+//object
+
+
+
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
