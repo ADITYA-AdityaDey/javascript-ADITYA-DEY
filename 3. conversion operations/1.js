84173a02e1f2cf1d1acffe90ad74b15eb362a1ea
ADITYA DEY <deyaditya2004@gmail.com>
2023-12-07 00:55:57 +0530

    conversion of data types

---
+++ 3. conversion operations/1.js
@@ -0,0 +1,28 @@
+let score = "ADITYA DEY"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "50" => 55
+//"50abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "ADITYA DEY"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+// 1 => true ; 0 => false
+// "" => false
+// "ADITYA DEY" => true
+
+let someNumber = 60
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+
+// 50 => String
