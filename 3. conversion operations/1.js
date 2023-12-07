9c0c592ecee7620fd0093b61a7477c6a94534bf0
ADITYA DEY <deyaditya2004@gmail.com>
2023-12-08 01:54:42 +0530

    OPERATIONS

--- 3. conversion operations/1.js
+++ 3. conversion operations/1.js
@@ -23,6 +23,51 @@ let someNumber = 60
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
 // 50 => String
+
+
+// ******************** OPERATIONS ***********************
+
+let value = 10
+let negValue = -value
+// console.log(negValue);
+
+// console.log(10+10);
+// console.log(10-10);
+// console.log(10*10);
+// console.log(10**10);
+// console.log(10/10);
+// console.log(9%10);
+
+let str1 = "ADITYA"
+let str2 = " DEY"
+
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4)*(5%3));
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 100
+
+// console.log(num1, num2, num3);
+
+
+let gameCounter = 500
+++gameCounter; //INCREMENT VALUE BY 1 OR ADD(+) 1  (500 => 500+1 => 501)
+console.log(gameCounter);
